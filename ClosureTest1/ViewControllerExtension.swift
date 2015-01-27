5e6d0807af553d5f953b5c379a0c7416fac0793f
Max Macleod <max@theappsworkshop.com>
2015-01-27 10:57:16 +0100

    Initial commit

---
+++ ClosureTest1/ViewControllerExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerExtension.swift
+//  ClosureTest1
+//
+//  Created by Developer on 27/01/2015.
+//  Copyright (c) 2015 TheAppsWorkshop. All rights reserved.
+//
+
+extension ViewController
+{
+    func myExtensionFunction()
+    {
+        
+    }
+}
