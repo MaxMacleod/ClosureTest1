0643572e658f17927654a5d122fc8767d049e4e6
Max Macleod <max@theappsworkshop.com>
2015-01-27 10:41:07 +0100

    Initial Commit

---
+++ ClosureTest1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClosureTest1
+//
+//  Created by Developer on 27/01/2015.
+//  Copyright (c) 2015 TheAppsWorkshop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
